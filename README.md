b00e30900434a2f977751c74292ee3918e8c832b
AzureAD\JeromeChallet <cjerome@premiumartsinc.com>
2024-02-06 10:17:32 +0900

    3rd commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-Number function convert to a string to a Number
+"Number" function convert to a string to a Number
 console.table();
+convert temperaturs
+calculate amplitude
--- script.js
+++ script.js
@@ -26,3 +26,33 @@ const measureKelvin = function () {
 };
 //A) identify the bug
 console.log(measureKelvin());
+
+//to add a debugger in google chrome
+//inspect -> sources -> select script.js -> place a break point on the line you want
+//now when we reload the page, the execution will stop at this point
+//at that point, we can see the values in the console log to make sure they are what they are supposed to be at that point
+//you can execute the rest of the funciton by hitting the "step" (F9)
+//this way youc an compare with the previous value you have just read
+
+//calculate amplitude
+const calcTempAmplitudeNew = function (t1, t2) {
+	const temp = t1.concat(t2);
+	console.log(temps);
+
+	let max = temps[0];
+	let min = temps[0];
+
+	for (let i = 0; i < temps.length; i++) {
+		const curTemp = temps[i];
+		if (typeof curTemp !== "number") continue;
+
+		if (curTemp > max) max = curTemp;
+		if (curTemp < min) min = curTemp;
+	}
+	console.log(max, min);
+	return max - min;
+};
+
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+
+console.log(amplitudeNew);
